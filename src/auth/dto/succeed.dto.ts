87304fffbd55ce8754b3286d5e676bd7463e6b1d
luoxiang <sunflowerinaries@gamil.com>
2021-12-12 17:53:21 +0800

    rm png

--- src/auth/dto/succeed.dto.ts
+++ src/auth/dto/succeed.dto.ts
@@ -4,7 +4,7 @@ import { ApiProperty } from '@nestjs/swagger';
 export class SucceedDto {
   @ApiProperty({
     description: '回复',
-    example: '1.验证码已发送至您的邮箱中请注意查收\n2.验证成功\n3.注册成功\n4.邮箱重置成功\n5.密码重置成功',
+    example: '1.验证码已发送至您的邮箱中请注意查收 2.验证成功 3.注册成功 4.邮箱重置成功 5.密码重置成功',
   })
   @IsString()
   message: string;
